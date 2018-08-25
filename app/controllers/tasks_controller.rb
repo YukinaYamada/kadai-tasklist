f99ec2eb500c4dbce75387902d8ff3d81b36179d
yukina.yamada <yukina6226@gmail.com>
2018-08-25 06:27:38 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,6 @@ class TasksController < ApplicationController
   end
   
   def show
-    set_task
   end
   
   def new
@@ -26,12 +25,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    set_task
   end 
 
   def update
-    set_task
-
     if @task.update(task_params)
       flash[:success] = '正常に投稿されました'
       redirect_to @task
@@ -42,7 +38,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    set_task
     @task.destroy
     
     flash[:success] = '削除されました'
